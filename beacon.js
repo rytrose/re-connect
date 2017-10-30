3775ec45f17c048a60d515c4cf520f38b01f45c9
Ryan Rose <rytrose@gmail.com>
2017-10-30 16:02:03 +0000

    original demo working again with VLC and mixing

--- beacon.js
+++ beacon.js
@@ -1,7 +1,10 @@
 var _ = require('lodash');
 var Bleacon = require('bleacon');
-var groove = require('groove');
 var osc = require("osc");
+
+/*
+	Setup OSC connection to python
+*/
 
 // Port listening on 7400
 var udp = new osc.UDPPort({
@@ -12,7 +15,10 @@ var udp = new osc.UDPPort({
 // Listen for incoming OSC messages.
 udp.on("message", function (oscMsg) {
     console.log("An OSC message was received!", oscMsg);
+    if(oscMsg.address == '/startBeacons') startBeacons();
 });
+
+udp.open();
 
 var sendOSCMessage = function (address, args) {
 	udp.send({
@@ -21,100 +27,81 @@ var sendOSCMessage = function (address, args) {
     }, "127.0.0.1", 7500); // Send to python at 7500
 };
 
-udp.on("ready", function () {
-	console.log("Sending test.");
-    sendOSCMessage('/test', ['Sent from js!']);
-});
+var beacons = {
+		'12424': {
+				'color': 'lemon', 
+				'song' : 'birds.mp3',
+				'proximity_buffer' : [],
+				'proximity' : 0,
+				'gain' : 0
+			 },
+		'51679': {
+				'color': 'candy', 
+				'song' : 'ocean.mp3',
+				'proximity_buffer' : [],
+				'proximity' : 0,
+				'gain' : 0
+			 },
+		'27465': {
+				'color': 'beetroot', 
+				'song' : 'rain.mp3',
+				'proximity_buffer' : [],
+				'proximity' : 0,
+				'gain' : 0
+			 }
+	};
 
-udp.open();
-
+sendOSCMessage("/setup", [JSON.stringify(beacons)]);
 
 /*
 	Beacon Scanning
 */
-var uuid = 'b9407f30f5f8466eaff925556b57fe6d';
-Bleacon.startScanning(uuid, 27465);
-var beacons = {
-	'12424': {
-			'color': 'lemon', 
-			'song' : 'birds.mp3',
-			'proximity_buffer' : [],
-			'proximity' : 0,
-			'gain' : 0,
-			'playlist' : groove.createPlaylist(),
-			'player' : groove.createPlayer()
-		 },
-	'51679': {
-			'color': 'candy', 
-			'song' : 'ocean.mp3',
-			'proximity_buffer' : [],
-			'proximity' : 0,
-			'gain' : 0,
-			'playlist' : groove.createPlaylist(),
-			'player' : groove.createPlayer()
-		 },
-	'27465': {
-			'color': 'beetroot', 
-			'song' : 'rain.mp3',
-			'proximity_buffer' : [],
-			'proximity' : 0,
-			'gain' : 0,
-			'playlist' : groove.createPlaylist(),
-			'player' : groove.createPlayer()
-		 }
-};
+var startBeacons = function	() {
+	console.log("Starting beacons.");
 
-var devices = groove.getDevices();
-var defaultDevice = devices[0];
+	var uuid = 'b9407f30f5f8466eaff925556b57fe6d';
+	Bleacon.startScanning(uuid);
 
-var playAudio = function(player, playlist) {
-	player.attach(playlist, function(err){
-		if(err) console.log(err);
-	});
-};
+	Bleacon.on('discover', function(bleacon) {
+		const AVG_SIZE = 10;
+		const PROX_THRESH = 0.2;
+		const GAIN_DEL = 0.05;
+		const ALPHA = 0.7;
+		var beacon = beacons[bleacon.major];
 
-var loadAudio = function() {
-	_.forEach(beacons, function(beacon){
-		beacon.player.device = defaultDevice;
-		groove.open(beacon.song, function(err, file) {
-			if(err) return console.log(err);
-			beacon.playlist.insert(file);
-			console.log('Loaded song ' + beacon.song + ' for ' + beacon.color);
-		});
-		beacon.playlist.setGain(0.0);
-		playAudio(beacon.player, beacon.playlist);
-	});
-};
+		if(beacon){
+			if(beacon.proximity_buffer.length < AVG_SIZE) beacon.proximity_buffer.push(bleacon.accuracy);
+			else {
+				beacon.proximity_buffer.shift();
+				beacon.proximity_buffer.push(bleacon.accuracy);
+			}
+			var oldProximity = beacon.proximity;
+			var newProximity = _.sum(beacon.proximity_buffer) / beacon.proximity_buffer.length;
+			// var newProximity = (ALPHA * newProximity) + ((1-ALPHA) * oldProximity);
+			var del = newProximity - oldProximity;
 
-loadAudio();
-
-var setVolume = function(beacon, proximity){
-	var gain = 1.3 - proximity;
-	beacon.playlist.setGain(Math.max(0,gain));
-};
-
-Bleacon.on('discover', function(bleacon) {
-	const AVG_SIZE = 10;
-	const PROX_THRESH = 0.2;
-	const GAIN_DEL = 0.05;
-	const ALPHA = 0.7;
-	var beacon = beacons[bleacon.major];
-
-	if(beacon){
-		if(beacon.proximity_buffer.length < AVG_SIZE) beacon.proximity_buffer.push(bleacon.accuracy);
-		else {
-			beacon.proximity_buffer.shift();
-			beacon.proximity_buffer.push(bleacon.accuracy);
+			if(Math.abs(del) < PROX_THRESH) setVolume(bleacon.major, newProximity);		
+			
+			beacon.proximity = newProximity;
+			//console.log('Found beacon: ' + beacon.color + ' at proximity ' + bleacon.accuracy + 'm away.'); 
 		}
-		var oldProximity = beacon.proximity;
-		var newProximity = _.sum(beacon.proximity_buffer) / beacon.proximity_buffer.length;
-		var newProximity = (ALPHA * newProximity) + ((1-ALPHA) * oldProximity);
-		var del = newProximity - oldProximity;
+	});
+}
 
-		if(Math.abs(del) < PROX_THRESH) setVolume(beacon, newProximity)		
-		console.log('Gain for ' + beacon.color + ': ' + beacon.playlist.gain);
+var setVolume = function (major, newProximity) {
+	var range1 = [0.3, 1.3];
+	var range2 = [100, 20];
+	var volume = 0;
 
-		beacon.proximity = newProximity;
-		//console.log('Found beacon: ' + beacon.color + ' at proximity ' + bleacon.accuracy + 'm away.'); 
-	}
-});
+	if(newProximity < range1[0]) volume = 100;
+	else if(newProximity > range1[1]) volume = 0;
+	else volume = convertRange(newProximity, range1, range2);
+
+	console.log('Proximity', newProximity, 'Volume', volume);
+
+	sendOSCMessage('/setGain', [major, volume]);
+}
+
+var convertRange = function( value, r1, r2 ) { 
+    return ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ];
+}
--- player.py
+++ player.py
@@ -1,5 +1,8 @@
 import threading
 import OSC
+import vlc
+import json
+
 
 class Player:
     def __init__(self):
@@ -10,19 +13,60 @@ class Player:
 
         self.beaconClient = OSC.OSCClient()
         self.beaconClient.connect(('127.0.0.1', 7400))
+        self.beaconServer.addMsgHandler("/setup", self.setupResponder)
+        self.beaconServer.addMsgHandler("/setGain", self.setGainResponder)
 
-        self.beaconServer.addMsgHandler("/test", self.testResponder)
+        self.beacons = {}
+        self.playing = ''
 
     def sendOSCMessage(self, addr, *msgArgs):
         msg = OSC.OSCMessage()
         msg.setAddress(addr)
         msg.append(*msgArgs)
         self.beaconClient.send(msg)
-        print "Sent message. -python"
 
-    def testResponder(self, addr, tags, stuff, source):
-        print stuff
-        self.sendOSCMessage('/test', ['Did you get this message?', 'Didja?'])
+    def setupResponder(self, addr, tags, stuff, source):
+        beacs = json.loads(stuff[0])  # dictionary of beacon information
+
+        for beacon in beacs:
+            self.beacons[beacon] = beacs[beacon]
+            vlc_instance = vlc.Instance()
+            player = vlc_instance.media_player_new()
+            media = vlc_instance.media_list_new([self.beacons[beacon]['song']])
+            mlp = vlc_instance.media_list_player_new()
+            mlp.set_media_player(player)
+            mlp.set_media_list(media)
+            mlp.set_playback_mode(vlc.PlaybackMode.loop)
+            self.beacons[beacon]['mlp'] = mlp
+            self.beacons[beacon]['player'] = player
+            self.playing = beacon
+
+        self.beacons[self.playing]['mlp'].play()
+        self.beacons[self.playing]['player'].audio_set_volume(0)
+        self.sendOSCMessage("/startBeacons", ["start"])
+
+    def setGainResponder(self, addr, tags, stuff, source):
+        beacon_major = stuff[0]
+        gain = stuff[1]
+        self.beacons[str(int(beacon_major))]['gain'] = gain
+
+        toPlay = ''
+        maxGain = -1
+
+        for major, beacon in self.beacons.iteritems():
+            if beacon['gain'] > maxGain:
+                toPlay = major
+                maxGain = beacon['gain']
+
+        if self.playing == toPlay:
+            print "Gain of " + self.beacons[self.playing]['song'] + ': ' + str(int(maxGain))
+            self.beacons[self.playing]['player'].audio_set_volume(int(maxGain))
+        else:
+            self.beacons[self.playing]['mlp'].pause()
+            self.playing = toPlay
+            self.beacons[self.playing]['player'].audio_set_volume(int(maxGain))
+            self.beacons[self.playing]['mlp'].play()
+            print "Gain of " + self.beacons[self.playing]['song'] + ': ' + str(int(maxGain))
 
 if __name__ == "__main__":
-	p = Player()
+    p = Player()
